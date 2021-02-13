99bf1a66b6ce7a8077526448ecce62d882e5ff71
Shashank Agarwal <shashank.agarwal@moonraft.com>
2021-02-14 01:50:56 +0530

    Random Commit

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -19,6 +19,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated=true;
     this.serverCreationStatus = 'Server was created, Name is ' + this.serverName;    
   }
 
